8627549944b16de50ef2a3d7acda1f98709a5b66
Postoak <129154578+brittpostoak@users.noreply.github.com>
2023-08-08 09:39:08 -0500

    Update shell.c

--- shell.c
+++ shell.c
@@ -1,37 +1,96 @@
-/* shell.c */
-
 #include <stdio.h>
 #include <stdlib.h>
-#include "shell.h"
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 
-/**
- * main - Entry point of the simple shell program.
- *
- * Return: Always 0.
- */
-int main(void)
-{
-	char *line;
-	char **commands;
-	int status = 1; /* Shell status (1: active, 0: exit) */
+#define LSH_TOK_BUFSIZE 64
+#define LSH_TOK_DELIM " \t\r\n\a"
 
-	while (status)
-	{
-		printf("($) ");
-		line = read_line(); /* Read input line */
-		if (!line)
-			break;
+char *lsh_read_line(void);
+char **lsh_split_line(char *line);
+int lsh_execute(char **args);
 
-		commands = split_commands(line); /* Split input into commands */
+int main(void) {
+    char *line;
+    char **args;
+    int status = 1;
+    int i; /* Declare 'i' outside of the loop in C89 */
 
-		if (commands)
-		{
-			status = execute_commands(commands); /* Execute commands */
-			free(commands);
-		}
-		free(line);
-	}
+    while (status) {
+        printf("($) ");
+        line = lsh_read_line();
+        if (!line)
+            break;
 
-	return (0);
+        args = lsh_split_line(line);
+        if (args) {
+            pid_t pid;
+            int exec_status;
+
+            pid = fork();
+            if (pid == 0) {
+                /* Child process */
+                if (execvp(args[0], args) == -1) {
+                    perror("shell");
+                }
+                exit(EXIT_FAILURE);
+            } else if (pid < 0) {
+                /* Error forking */
+                perror("shell");
+            } else {
+                /* Parent process */
+                do {
+                    waitpid(pid, &exec_status, WUNTRACED);
+                } while (!WIFEXITED(exec_status) && !WIFSIGNALED(exec_status));
+            }
+
+            for (i = 0; args[i] != NULL; i++) {
+                free(args[i]);
+            }
+            free(args);
+        }
+        free(line);
+    }
+
+    return 0;
+}
+
+char *lsh_read_line(void) {
+    char *line = NULL;
+    size_t bufsize = 0;
+    getline(&line, &bufsize, stdin);
+    return line;
+}
+
+char **lsh_split_line(char *line) {
+    int bufsize = LSH_TOK_BUFSIZE, position = 0;
+    char **tokens = malloc(bufsize * sizeof(char *));
+    char *token;
+
+    if (!tokens) {
+        fprintf(stderr, "shell: allocation error\n");
+        exit(EXIT_FAILURE);
+    }
+
+    token = strtok(line, LSH_TOK_DELIM);
+    while (token != NULL) {
+        tokens[position] = token;
+        position++;
+
+        if (position >= bufsize) {
+            bufsize += LSH_TOK_BUFSIZE;
+            tokens = realloc(tokens, bufsize * sizeof(char *));
+            if (!tokens) {
+                fprintf(stderr, "shell: allocation error\n");
+                exit(EXIT_FAILURE);
+            }
+        }
+
+        token = strtok(NULL, LSH_TOK_DELIM);
+    }
+    tokens[position] = NULL;
+    return tokens;
 }
 
